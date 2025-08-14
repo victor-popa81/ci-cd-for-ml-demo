65fa4ec9b5898f1277f69de23e254b11f9260453
victor-popa81 <victor.popa81@gmail.com>
2025-08-14 10:51:34 +0200

    Create hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+import datetime
+
+def main():
+  print("Hello World")
+
+  # get current time
+  current_time = datetime.datetime.now()
+  print("Current time: ", current_time)
+
+if __name__=="__main__":
+  main()
