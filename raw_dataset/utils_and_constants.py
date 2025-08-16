a00b2c565e93ec7393a46b59b06a55b3ceed9a1b
victor-popa81 <victor.popa81@gmail.com>
2025-08-16 16:54:15 +0200

    advance from main by only changing RFC_FOREST_DEPTH to 10

--- raw_dataset/utils_and_constants.py
+++ raw_dataset/utils_and_constants.py
@@ -6,7 +6,7 @@ DROP_COLNAMES = ["Date"]
 TARGET_COLUMN = "RainTomorrow"
 RAW_DATASET = "raw_dataset/weather.csv"
 PROCESSED_DATASET = "processed_dataset/weather.csv"
-RFC_FOREST_DEPTH = 2
+RFC_FOREST_DEPTH = 10
 
 
 def delete_and_recreate_dir(path):
