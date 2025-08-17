92a04f453d9cba5fd653328da864fca0114708e0
victor-popa81 <victor.popa81@gmail.com>
2025-08-17 18:14:55 +0200

    hiperparamenter tuning baseline

--- raw_dataset/metrics_and_plots.py
+++ raw_dataset/metrics_and_plots.py
@@ -17,3 +17,12 @@ def save_metrics(metrics):
 def save_predictions(y_test, y_pred):
     df = pd.DataFrame({'true_label': y_test, 'predicted_label': y_pred})
     df.to_csv("predictions.csv")
+
+def save_roc_curve(y_test, y_pred_proba):
+    # Calcualte ROC curve
+    fpr, tpr, _ = roc_curve(y_test, y_pred_proba[:, 1])
+    # Store roc curve data
+    cdf = pd.DataFrame(np.column_stack([fpr, tpr]), columns=["fpr", "tpr"]).astype(
+        float
+    )
+    cdf.to_csv("roc_curve.csv", index=None)
--- raw_dataset/utils_and_constants.py
+++ raw_dataset/utils_and_constants.py
@@ -16,3 +16,35 @@ def delete_and_recreate_dir(path):
         pass
     finally:
         Path(path).mkdir(parents=True, exist_ok=True)
+
+def load_data(file_path):
+    data = pd.read_csv(file_path)
+    X = data.drop(TARGET_COLUMN, axis=1)
+    y = data[TARGET_COLUMN]
+    return X, y
+
+
+def load_hyperparameters(hyperparameter_file):
+    with open(hyperparameter_file, "r") as json_file:
+        hyperparameters = json.load(json_file)
+    return hyperparameters
+
+
+def get_hp_tuning_results(grid_search: GridSearchCV) -> str:
+    """Get the results of hyperparameter tuning in a Markdown table"""
+    cv_results = pd.DataFrame(grid_search.cv_results_)
+
+    # Extract and split the 'params' column into subcolumns
+    params_df = pd.json_normalize(cv_results["params"])
+
+    # Concatenate the params_df with the original DataFrame
+    cv_results = pd.concat([cv_results, params_df], axis=1)
+
+    # Get the columns to display in the Markdown table
+    cv_results = cv_results[
+        ["rank_test_score", "mean_test_score", "std_test_score"]
+        + list(params_df.columns)
+    ]
+
+    cv_results.sort_values(by="mean_test_score", ascending=False, inplace=True)
+    return cv_results.to_markdown(index=False)
